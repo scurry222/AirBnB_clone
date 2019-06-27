dbbb4beffab649514a08c80fd2e121522a82b6e1
George Solorio <George_Solorio@yahoo.com>
2019-06-27 23:00:08 +0000

    New methods added for saving data

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 import uuid
+
 from datetime import datetime
+from models.__init__ import storage
 
 """
 base_model - 1 class
@@ -25,6 +27,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
@@ -42,6 +45,7 @@ class BaseModel():
     def save(self):
         """ Update the time """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """ Return a dict representation """
