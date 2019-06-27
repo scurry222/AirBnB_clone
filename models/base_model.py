de18bf80bd403f112ff61e7e0853aa436e727a17
scurry222 <scurry222@gmail.com>
2019-06-27 18:22:43 +0000

    pep8 compliant

--- models/base_model.py
+++ models/base_model.py
@@ -4,17 +4,25 @@ import uuid
 import datetime
 
 """
+base_model - 1 class
 """
 
 
 class BaseModel():
     """
+    Class for the models to build on
     """
     id = str(uuid.uuid4())
     created_at = datetime.datetime.now()
     updated_at = datetime.datetime.now()
 
     def __str__(self):
+        """
+        Make string representation of the class
+
+        Args:
+            self('class'): object to represent
+        """
         string = ""
         string += "[<{}>] ".format(__class__.__name__)
         string += "(<{}>) ".format(self.id)
@@ -22,12 +30,14 @@ class BaseModel():
         return string
 
     def save(self):
+        """ Update the time """
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        tmp = ""
-        tmp = self.__dict__.copy()
-        tmp['__class__'] = self.__class__.__name__
-        tmp['created_at'] = self.created_at.isoformat()
-        tmp['updated_at'] = self.updated_at.isoformat()
-        return tmp
+        """ Return a dict representation """
+        d = ""
+        d = self.__dict__.copy()
+        d['__class__'] = self.__class__.__name__
+        d['created_at'] = self.created_at.isoformat()
+        d['updated_at'] = self.updated_at.isoformat()
+        return d
